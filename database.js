5c77dfb8bd84f7129968c22f277312e599211dfc
justusmattern <50420447+justusmattern@users.noreply.github.com>
2020-09-14 11:15:40 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,5 @@
 // ---------- MONGODB CONNECTION --------- //
 
-/**const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://superuser:banc1wOwS@satellitescluster.jwazr.mongodb.net/satellites?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-module.exports = client;**/
 
 const MongoClient = require( 'mongodb' ).MongoClient;
 const uri = "mongodb+srv://superuser:<password>@satellitescluster.jwazr.mongodb.net/satellites?retryWrites=true&w=majority";
